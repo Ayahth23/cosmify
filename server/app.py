975ec8cab1380af31eb31b63d32d2c8067eee926
Matthew Youngbar <matthewyoungbar@gmail.com>
2022-11-05 17:19:08 -0400

    Add Fact of the Day Endpoint

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import requests
 import nasapy
 from datetime import datetime
@@ -39,3 +40,18 @@ def pic_of_day():
     #If media type is not image:
     else:
         print("Sorry, Image not available!")
+
+@app.route('/fact-of-day', methods=['GET'])
+def fact_of_day():
+    facts = ["Space does not begin at a specific altitude above the Earth, but the Kármán line at 100 km is a commonly used definition.",
+            "The temperature in the void of space is about −270.45 °C.", "Space is a hard vacuum, meaning it is a void containing very little matter.",
+            "There is no sound in space because molecules are too far apart to transmit sound.",
+            "The space between galaxies is not completely empty but has an average of one atom per cubic meter.",
+            "There are an estimated 100-400 billion stars in our galaxy, the Milky Way.",
+            "The universe is observed to be 13.8 billion years old and has been expanding since its formation in the Big Bang."
+            "In the observable universe there are an estimated 2 trillion (2,000,000,000,000) galaxies."
+            "The International Space Station is the largest ever crewed object in space.",
+            "Spacecraft have visited all the known planets in our solar system."]
+    day = int((time.time()//86400)%len(facts))
+    
+    return json.dumps({"fact": facts[day]})
