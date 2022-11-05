dfd343068324cac6a7d847b4012495dfab19c7cc
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-11-05 19:41:11 -0400

    show image from link and remove padding

--- Cosmify/app/src/main/java/com/example/cosmify/Widget.kt
+++ Cosmify/app/src/main/java/com/example/cosmify/Widget.kt
@@ -2,18 +2,49 @@ package com.example.cosmify
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.graphics.Bitmap
 import android.widget.RemoteViews
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.AppWidgetTarget
+import com.bumptech.glide.request.transition.Transition
+
 
 /**
  * Implementation of App Widget functionality.
  */
 class Widget : AppWidgetProvider() {
+    fun pushWidgetUpdate(context: Context, remoteViews: RemoteViews?) {
+        val myWidget = ComponentName(context, AppWidgetProvider::class.java)
+        val manager = AppWidgetManager.getInstance(context)
+        manager.updateAppWidget(myWidget, remoteViews)
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+
+
+//        pushWidgetUpdate(context, remoteViews)
+        val remoteViews = RemoteViews(context.packageName, R.layout.widget)
+
+        val appWidgetTarget =
+            object : AppWidgetTarget(context, R.id.appwidget_img, remoteViews, *appWidgetIds) {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap?>?) {
+                    super.onResourceReady(resource!!, transition)
+                }
+            }
+
+        Glide
+            .with(context.applicationContext)
+            .asBitmap()
+            .load("https://apod.nasa.gov/apod/image/2211/Lunar-Eclipse-South-Pole.jpg")
+            .override(480,320)
+            .into(appWidgetTarget)
+
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
