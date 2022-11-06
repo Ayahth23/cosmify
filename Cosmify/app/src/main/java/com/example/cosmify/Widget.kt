a289e197e4ba969edb03e3cfda5bd86a61dc95ee
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-11-05 22:50:08 -0400

    add local date and time to widget

--- Cosmify/app/src/main/java/com/example/cosmify/Widget.kt
+++ Cosmify/app/src/main/java/com/example/cosmify/Widget.kt
@@ -5,10 +5,18 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.graphics.Bitmap
+import android.util.Log
 import android.widget.RemoteViews
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.AppWidgetTarget
 import com.bumptech.glide.request.transition.Transition
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 /**
@@ -38,12 +46,22 @@ class Widget : AppWidgetProvider() {
                 }
             }
 
-        Glide
-            .with(context.applicationContext)
-            .asBitmap()
-            .load("https://apod.nasa.gov/apod/image/2211/Lunar-Eclipse-South-Pole.jpg")
-            .override(480,320)
-            .into(appWidgetTarget)
+        val thread = Thread {
+            try {
+                val picDay = JSONObject(URL("https://cosmify-367717.uk.r.appspot.com/space-picture").readText())
+                val imgURL = picDay.getString("imageURL")
+                Glide
+                    .with(context.applicationContext)
+                    .asBitmap()
+                    .load(imgURL)
+                    .override(480,320)
+                    .into(appWidgetTarget)
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        thread.start()
 
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
@@ -65,11 +83,27 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    val thread = Thread {
+        try {
+            val rocketLaunch =
+                JSONObject(URL("https://cosmify-367717.uk.r.appspot.com/next-launch").readText())
+            val nextLaunch = rocketLaunch.getString("win_open")
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+//            val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")
+            val zonedDateTime = ZonedDateTime.parse(nextLaunch)
+            val newTime = zonedDateTime.withZoneSameInstant(ZoneId.of(TimeZone.getDefault().id))
+            val ret = DateTimeFormatter.ofPattern("MM/dd/yyyy - hh:mm").format(newTime)
+
+//            val widgetText = context.getString(R.string.appwidget_text)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.widget)
+            views.setTextViewText(R.id.appwidget_text, ret)
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        } catch (exc: Exception) {
+            Log.e("Exception", exc.toString())
+        }
+    }
+
+    thread.start()
 }
